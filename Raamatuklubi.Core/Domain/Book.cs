5b9686a059305dff95ca437fa0929b323e7264b9
Opilane TTHK <opilane@tthk.ee>
2026-04-01 11:36:30 +0300

    Book and book comment tables

---
+++ Raamatuklubi.Core/Domain/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raamatuklubi.Core.Domain
+{
+    public class Book
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public Guid UserAddedID { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<Comment> Comments { get; set; }
+        public DateTime EntryCreatedAt { get; set; }
+        public DateTime EntryModifiedAt { get; set; }
+    }
+}
