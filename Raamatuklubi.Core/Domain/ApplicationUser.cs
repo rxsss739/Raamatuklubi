982b37028f7b7fd2e8b67084b0e70113d3ae2147
opilane <opilane@HP-CZC3217069>
2026-04-24 13:40:05 +0300

    ApplicationUser table

---
+++ Raamatuklubi.Core/Domain/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raamatuklubi.Core.Domain
+{
+    public class ApplicationUser
+    {
+        public List<Book> AddedBooks { get; set; }
+        public List<Comment> AddedComments { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
