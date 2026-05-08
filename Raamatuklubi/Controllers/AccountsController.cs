884c4ec2e6440ae6e65ec8664630932dd914c76c
rxsss739 <189347959+rxsss739@users.noreply.github.com>
2026-05-08 11:27:10 +0300

    Made RegisterViewModel. Added buttons to shared layout. Added accountscontroller with Register post and get

---
+++ Raamatuklubi/Controllers/AccountsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Raamatuklubi.Core.Domain;
+using Raamatuklubi.Data;
+using Raamatuklubi.Models.Accounts;
+
+namespace Raamatuklubi.Controllers
+{
+    public class AccountsController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RaamatuklubiDbContext _context;
+
+        public AccountsController
+            (
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RaamatuklubiDbContext context
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    IsAdmin = model.IsAdmin,
+                    DisplayName = model.DisplayName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Raamatuklubi/Models/Accounts/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Raamatuklubi.Models.Accounts
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Paroolid ei kattu, kontrolli et oled sama moodi sisestanud")]
+        [Display(Name = "Sisesta parool uuesti:")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public string? DisplayName { get; set; }
+        public bool IsAdmin { get; set; } = false;
+    }
+}
