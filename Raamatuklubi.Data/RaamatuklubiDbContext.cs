25621a84d42756ebcdd2555e5adc13ae9c757422
opilane <opilane@HP-CZC3217069>
2026-05-08 10:17:21 +0300

    a

--- Raamatuklubi.Data/RaamatuklubiDbContext.cs
+++ Raamatuklubi.Data/RaamatuklubiDbContext.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Raamatuklubi.Core.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Raamatuklubi.Data
 {
-    public class RaamatuklubiDbContext : DbContext
+    public class RaamatuklubiDbContext : IdentityDbContext<ApplicationUser>
     {
         public RaamatuklubiDbContext(DbContextOptions<RaamatuklubiDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Comment> Comments { get; set; }
     }
 }
