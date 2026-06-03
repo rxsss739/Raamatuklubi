d092bf394703d6c940eac51f706a88d9811ed2c3
rxsss739 <189347959+rxsss739@users.noreply.github.com>
2026-06-03 09:16:47 +0300

    Add EventDescription and card layout for events
    Styled ongoing events page, now events are displayed as cards

--- Raamatuklubi/Models/BookClubs/OngoingEventsViewModel.cs
+++ Raamatuklubi/Models/BookClubs/OngoingEventsViewModel.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public string EventName { get; set; }
+        public string EventDescription { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public List<string> Attendees { get; set; }
