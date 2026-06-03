e3580b170a1cdbc4ab4fd0dafc58bfb037f1159a
rxsss739 <189347959+rxsss739@users.noreply.github.com>
2026-06-03 10:12:08 +0300

    made eventdetailsviewmodel and eventdetails view. edited ongoingevents view so it can redirect from each card to eventdetails

---
+++ Raamatuklubi/Models/BookClubs/EventDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Raamatuklubi.Models.BookClubs
+{
+    public class EventDetailsViewModel
+    {
+        public Guid Id { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Location { get; set; }
+        public List<string> Attendees { get; set; }
+    }
+}
